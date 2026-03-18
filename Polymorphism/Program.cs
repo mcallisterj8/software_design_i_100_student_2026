5c31d71ddc912d60d383c224833b9da9b183ae3d
Jacob McAllister <mcallisterj@westerntc.edu>
2026-03-18 09:50:58 -0500

    Update
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello World!");
+﻿Console.WriteLine("\n================= Compile Time & Runtime Types ================\n");
+Vehicle vehicleOne = new Vehicle();
+Airplane airplaneOne = new Airplane("Delta", 35000, 2);
+Automobile carOne = new Automobile("ABC123", 5, true);
+Boat boatOne = new Boat(30.5, true, "Yacht");
+
+List<Airplane> planes = new List<Airplane>();
+
+
+
