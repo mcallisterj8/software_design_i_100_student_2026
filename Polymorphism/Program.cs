86e095c3c11c59db2e16dd1d7e19032236d0cb61
Jacob McAllister <mcallisterj@westerntc.edu>
2026-03-18 10:37:52 -0500

    End of class
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -4,7 +4,28 @@ Airplane airplaneOne = new Airplane("Delta", 35000, 2);
 Automobile carOne = new Automobile("ABC123", 5, true);
 Boat boatOne = new Boat(30.5, true, "Yacht");
 
-List<Airplane> planes = new List<Airplane>();
+Vehicle mysteryBox = new Automobile("ABC123", 5, true);
+Automobile test = new ElectricCar("BCD456", 3, 100);
+mysteryBox.Drive();
+mysteryBox = new Airplane("Delta", 35000, 2);
 
+if(mysteryBox is Automobile) {
+    ((Automobile)mysteryBox).Honk();
+} else {
+    Console.WriteLine("Automobile is not in mysteryBox.");
+}
+
+mysteryBox.Drive();
+
+List<Vehicle> vehicles = new List<Vehicle> {
+    vehicleOne,
+    airplaneOne,
+    carOne,
+    boatOne
+};
+
+// foreach(Vehicle elem in vehicles) {    
+//     elem.Drive();
+// }
 
 
