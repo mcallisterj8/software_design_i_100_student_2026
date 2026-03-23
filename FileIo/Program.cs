c6d2f0edf9567027c10dec51bb8dbbc3c6b04d36
Jacob McAllister <mcallisterj@westerntc.edu>
2026-03-23 10:16:22 -0500

    Update
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

---
+++ FileIo/Program.cs
@@ -0,0 +1,43 @@
+﻿
+/**************************************
+    READING FROM TEXT FILE
+**************************************/
+string filePath = "./lorem_ipsum.txt";
+string fileContents = File.ReadAllText(filePath);
+
+Console.WriteLine($"\nFile Contents:\n\n{fileContents}\n");
+
+/**************************************
+    WRITING TO TEXT FILE
+**************************************/
+string pathToFile = $"{Directory.GetCurrentDirectory()}/hello.txt";
+string content = "Hello World!";
+
+File.WriteAllText(pathToFile, content);
+
+// Console.WriteLine($"\n\nCurrent Directory:\n{Directory.GetCurrentDirectory()}\n\n");
+
+string moreContent = "\nThe sun is out!";
+File.AppendAllText(pathToFile, moreContent);
+
+/**************************************
+    FORMAT WRITING TO TEXT FILE
+**************************************/
+string pathToFormatFile = "./format.txt";
+string title = "Example Title";
+int number = 80;
+
+string formatContent =
+    @$"{title}
+================
+Non-indented sentence.
+    Indented sentence with the number {number}!";
+
+File.WriteAllText(pathToFormatFile, formatContent);
+
+
+/**************************************
+    READING FROM JSON
+**************************************/
+
+
